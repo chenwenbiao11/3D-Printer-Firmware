22f0dcbc6d5f6d6d258f9affed5f41474b2ad71b
Wenbiao Chen <2148244129@qq.com>
2021-07-26 15:39:44 +0800

    Change something

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -143,7 +143,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "chenwenbiao's 3D Printer"
+#define CUSTOM_MACHINE_NAME "3D Printer"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
